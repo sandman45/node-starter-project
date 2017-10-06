1fc19badf95fbbc344d9e31cb684afc09a164871
sandman45 <msanders45@gmail.com>
2017-10-06 10:13:40 -0600

    express validator

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const helmet = require('helmet');
 
 const config = require('./server/config/config');
 const middleware = require('./server/middleware');
+const validator = require('express-validator');
 
 global.logger = winston;
 const logger = global.logger;
@@ -17,6 +18,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
+app.use(validator());
 app.use(middleware.accessControl);
 
 // routes
--- server/routes/main/exampleRoute.js
+++ server/routes/main/exampleRoute.js
@@ -1,23 +1,40 @@
+const logger = global.logger;
 
 const auth = require('../../../server/middleware/security/auth');
 const apiRoutes = require('../../../server/routes/routes');
 const routesVersioning = require('express-routes-versioning')();
+const { check, validationResult } = require('express-validator/check');
 
 const exampleModule = require('../../../server/modules/exampleModule/example');
 const exampleModule2 = require('../../../server/modules/exampleModule/example2');
 
 const errors = require('../../middleware/response/lib/errorIndex');
 
+const exampleValidator = [
+    check('id').exists().withMessage('id is a required attribute').isMongoId()
+    .withMessage('Must be a MongoId.'),
+];
+
 module.exports = (app) => {
-    app.get(apiRoutes.test, (req, res, next) => {
-        exampleModule.test('my awesome test route').then((results) => {
-            next(results);
-        }).catch((err) => {
-            next(err);
-        });
+    app.get(apiRoutes.test, exampleValidator, (req, res, next) => {
+        logger.info(`[${apiRoutes.test}] invoked`);
+        const validationRes = validationResult(req);
+        let error = null;
+        if (!validationRes.isEmpty()) {
+            error = new errors.BadRequestResponse(400, validationRes.mapped());
+            logger.debug(req.requestId, JSON.stringify(error));
+            next(error);
+        } else {
+            exampleModule.test('my awesome test route').then((results) => {
+                next(results);
+            }).catch((err) => {
+                next(err);
+            });
+        }
     });
 
-    app.get(apiRoutes.test2, (req, res, next) => {
+    app.get(apiRoutes.test2, auth.authCheck, (req, res, next) => {
+        logger.info(`[${apiRoutes.test2}] invoked`);
         exampleModule.test2('my awesome test route 2').then((results) => {
             next(results);
         }).catch((err) => {
@@ -25,7 +42,8 @@ module.exports = (app) => {
         });
     });
 
-    app.get(apiRoutes.test3, (req, res, next) => {
+    app.get(apiRoutes.test3, auth.authCheck, (req, res, next) => {
+        logger.info(`[${apiRoutes.test3}] invoked`);
         exampleModule2.test3('my awesome test route 3').then((results) => {
             next(results);
         }).catch((err) => {
@@ -34,6 +52,7 @@ module.exports = (app) => {
     });
 
     app.get(apiRoutes.error, (req, res, next) => {
+        logger.info(`[${apiRoutes.error}] invoked`);
         const error = new errors.BadRequestResponse(500, 'This threw an error.');
         next(error);
     });
